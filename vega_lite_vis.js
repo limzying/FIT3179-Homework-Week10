fda8a7361dff4d63e2e4338c0cd22f439ad524db
Zying <114282459+limzying@users.noreply.github.com>
2022-10-08 15:34:26 +0800

    remove action button

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,7 +2,7 @@ var vg_1="worldwide_internet_detail.vg.json";
 var vg_2="worldwide_internet_user.vg.json";
 
 vegaEmbed("#scatter_plot",vg_1).then(function(result){
-
+  "action":false
 }).catch(console.error);
 
 vegaEmbed("#choropleth_map",vg_2).then(function(result){
